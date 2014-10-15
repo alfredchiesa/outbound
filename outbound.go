40baa39724bb0e3f924952e376314eb933ff0146
Alfred Chiesa <achiesa@riotgames.com>
2014-10-15 12:02:37 -0700

    more progress

--- outbound.go
+++ outbound.go
@@ -1,85 +1,150 @@
-package main
+package outbound
 
-import "net/http"
-
-// Some basic structs for now
-type Response struct {
-	StatusCode int
-	Status     string
-	Body       string
-}
+import (
+	"bufio"
+	"bytes"
+	"compress/flate"
+	"compress/gzip"
+	"compress/zlib"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/url"
+	"reflect"
+	"strings"
+	"time"
+)
 
 type Request struct {
-	Uri         string
-	Method      string
-	Body        string
-	UserAgent   string
-	ContentType string
-	Host        string
-	Headers     []string
+	headers           []headerTuple
+	Method            string
+	Uri               string
+	Body              interface{}
+	QueryString       interface{}
+	Timeout           time.Duration
+	ContentType       string
+	Accept            string
+	Host              string
+	UserAgent         string
+	Insecure          bool
+	MaxRedirects      int
+	Proxy             string
+	Compression       *compression
+	BasicAuthUsername string
+	BasicAuthPassword string
 }
 
-// Struct method testing
-//
-// trying methods on the Request struct. This might be cleaner for when
-// it comes time to start passing values. instead of doing kwargs of interfaces
-func (req Request) Send(*Response, error) {
+type Response struct {
+	StatusCode    int
+	ContentLength int64
+	Body          *Body
+	Header        http.Header
+}
 
-	client := &http.Client{}
+type Body struct {
+	reader           io.ReadCloser
+	compressedReader io.ReadCloser
+}
 
-	r, err := http.NewRequest(req.Method, req.Uri, nil)
+type Error struct {
+	timeout bool
+	Err     error
+}
 
-	if err != nil {
+func (e *Error) Timeout() bool {
+	return e.timeout
+}
+
+func (e *Error) Error() string {
+	return e.Err.Error()
+}
+
+func (b *Body) Read(p []byte) (int, error) {
+	if b.compressedReader != nil {
+		return b.compressedReader.Read(p)
+	}
+	return b.reader.Read(p)
+}
+
+func (b *Body) Close() error {
+	err := b.reader.Close()
+	if b.compressedReader != nil {
+		return b.compressedReader.Close()
+	}
+	return err
+}
+
+func paramParse(query interface{}) (string, error) {
+	var (
+		v = &url.Values{}
+		s = reflect.ValueOf(query)
+		t = reflect.TypeOf(query)
+	)
+
+	switch query.(type) {
+	case url.Values:
+		return query.(url.Values).Encode(), nil
+	default:
+		for i := 0; i < s.NumField(); i++ {
+			v.Add(strings.ToLower(t.Field(i).Name), fmt.Sprintf("%v", s.Field(i).Interface()))
+		}
+		return v.Encode(), nil
+	}
+}
+
+func prepareRequestBody(b interface{}) (io.Reader, error) {
+	switch b.(type) {
+	case string:
+		return strings.NewReader(b.(string)), nil
+	case io.Reader:
+		return b.(io.Reader), nil
+	case []byte:
+		return bytes.NewReader(b.([]byte)), nil
+	case nil:
+		return nil, nil
+	default:
+		j, err := json.Marshal(b)
+		if err == nil {
+			return bytes.NewReader(j), nil
+		}
 		return nil, err
 	}
+}
+func (r Request) Send() (*Response, error) {
+	var req *http.Request
+	var er error
+	var transport = defaultTransport
+	var client = defaultClient
 
-	resp, err := client.Do(r)
-
-	if err != nil {
-		return nil, err
+	if r.QueryString != nil {
+		param, e := paramParse(r.QueryString)
+		if e != nil {
+			return nil, &Error{Err: e}
+		}
+		r.Uri = r.Uri + "?" + param
 	}
 
-	return resp, nil
-}
+	var bodyReader io.Reader
+	req, er = http.NewRequest(r.Method, r.Uri, bodyReader)
 
-// Public Func Prototypes
-// these will be like outbound.GET, outbound.PUT, outbound.UDP
-func GET(...interface{}) {
-	//pass
-}
+	if er != nil {
+		return nil, &Error{Err: er}
+	}
 
-func POST(...interface{}) {
-	//pass
-}
+	res, err := client.Do(req)
+	if timer != nil {
+		timer.Stop()
+	}
 
-func PUT(...interface{}) {
-	//pass
-}
+	if isRedirect(res.StatusCode) && r.MaxRedirects > 0 {
+		loc, _ := res.Location()
+		r.MaxRedirects--
+		r.Uri = loc.String()
+		return r.Send()
+	}
 
-func DELETE(...interface{}) {
-	//pass
-}
-
-func HEAD(...interface{}) {
-	//pass
-}
-
-func OPTIONS(...interface{}) {
-	//pass
-}
-
-func PATCH(...interface{}) {
-	//pass
-}
-
-func UDP(...interface{}) {
-	//pass
-}
-
-// main loop to test things. won't be here when live
-func main() {
-	res, err := Request{
-		Method: "GET",
-		Uri:    "http://www.villaroad.com",
-	}.Send()
 }
